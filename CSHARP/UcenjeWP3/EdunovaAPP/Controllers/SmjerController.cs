b4d37f0aa2ae695d3df8655d2d5cfbc1fb5a5bc9
lcrljic <lukacrljic7@gmail.com>
2024-03-07 20:50:27 +0100

    CRUD

--- CSHARP/UcenjeWP3/EdunovaAPP/Controllers/SmjerController.cs
+++ CSHARP/UcenjeWP3/EdunovaAPP/Controllers/SmjerController.cs
@@ -1,4 +1,5 @@
 ﻿using EdunovaAPP.Data;
+using EdunovaAPP.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EdunovaAPP.Controllers
@@ -21,6 +22,44 @@ namespace EdunovaAPP.Controllers
         public IActionResult Get()
         {
             return new JsonResult(_context.Smjerovi.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Smjer smjer)
+        {
+            _context.Smjerovi.Add(smjer);
+            _context.SaveChanges(); 
+            return new JsonResult(smjer);
+        }
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        public IActionResult Put(int sifra, Smjer smjer) 
+        {
+            var smjerIzBaze = _context.Smjerovi.Find(sifra);
+            // Za sada ručno, kasnije će doći Mapper
+            smjerIzBaze.Naziv = smjer.Naziv;
+            smjerIzBaze.Trajanje = smjer.Trajanje;
+            smjerIzBaze.Cijena = smjer.Cijena;
+            smjerIzBaze.Verificiran = smjer.Verificiran;
+
+            _context.Smjerovi.Update(smjerIzBaze);
+            _context.SaveChanges();
+
+            return new JsonResult(smjerIzBaze);
+
+
+        }
+
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+        public IActionResult Delete(int sifra)
+        {
+            var smjerIzBaze = (_context.Smjerovi.Find(sifra));
+            _context.Smjerovi.Remove(smjerIzBaze);
+            _context.SaveChanges();
+            return new JsonResult(new { poruka = "Obrisano" });
         }
     }
 }
